73520df685fb11371e4968e402a29d465dab76f0
sanjaydevang <sanjayvdevang067@gmail.com>
2025-05-22 18:51:20 -0400

    date is working

--- app.js
+++ app.js
@@ -1,38 +1,37 @@
 function handleEditProfile() {
-    var name = document.getElementById("name").textContent
-    var inputName = document.getElementById("input-name")
-    inputName.value = name
+    // Show edit view first
+    document.getElementById("edit-view").style.display = "block";
+    document.getElementById("display-view").style.display = "none";
 
-    var email = document.getElementById("email").textContent
-    var inputEmail = document.getElementById("input-email")
-    inputEmail.value = email
+    // Transfer values
+    document.getElementById("input-name").value = document.getElementById("name").textContent;
+    document.getElementById("input-email").value = document.getElementById("email").textContent;
+    document.getElementById("input-dob").value = document.getElementById("dob-display").value;
+    document.getElementById("input-interests").value = document.getElementById("interests").textContent;
 
-    var interests = document.getElementById("interests").textContent
-    var inputInterests = document.getElementById("input-interests")
-    inputInterests.value = interests
-
-    document.getElementById("edit-view").style.display = "block"
-    document.getElementById("display-view").style.display = "none"
+    // Init datepicker after showing the input
+    new Datepicker(document.getElementById('input-dob'), {
+        autohide: true,
+        format: 'yyyy-mm-dd'
+    });
 }
 
 function handleUpdateProfile() {
-    var updatedName = document.getElementById("input-name").value
-    var name = document.getElementById("name")
-    name.textContent = updatedName
+    const updatedName = document.getElementById("input-name").value;
+    const updatedEmail = document.getElementById("input-email").value;
+    const updatedDOB = document.getElementById("input-dob").value;
+    const updatedInterests = document.getElementById("input-interests").value;
 
-    var updatedEmail = document.getElementById("input-email").value
-    var email = document.getElementById("email")
-    validator.isEmail(updatedEmail); //=> true
-    if (validator.isEmail(updatedEmail)) {
-        email.textContent = updatedEmail
-    } else {
-        alert("Please enter a valid email address.")
+    if (!validator.isEmail(updatedEmail)) {
+        alert("Please enter a valid email address.");
         return;
     }
-    var updatedInterests = document.getElementById("input-interests").value
-    var interests = document.getElementById("interests")
-    interests.textContent = updatedInterests
 
-    document.getElementById("edit-view").style.display = "none"
-    document.getElementById("display-view").style.display = "block"
+    document.getElementById("name").textContent = updatedName;
+    document.getElementById("email").textContent = updatedEmail;
+    document.getElementById("dob-display").value = updatedDOB;
+    document.getElementById("interests").textContent = updatedInterests;
+
+    document.getElementById("edit-view").style.display = "none";
+    document.getElementById("display-view").style.display = "block";
 }

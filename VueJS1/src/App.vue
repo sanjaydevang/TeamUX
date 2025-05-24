09aa75335c030c206602375a98fdd90d9677312d
sanjaydevang <sanjayvdevang067@gmail.com>
2025-05-24 11:04:35 -0400

    Vue 2nd

--- VueJS1/src/App.vue
+++ VueJS1/src/App.vue
@@ -1,15 +1,47 @@
 <template>
-  <div>
-    <h1>My Cool Vue App</h1>
-    <button>Add Task</button>
+  <div class="container">
+    <Header title="Vue App" action="Get started" color="lightblue" />
+  </div>
+  <div class="container">
+    <Header title="Todo list" action="Add Task" />
   </div>
 </template>
 
 <script>
+import Header from './Header.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header
+  }
 }
 </script>
 
+
+
+
 <style>
+#app{    
+  text-align: center;
+    color: brown;
+    margin: 60px;
+    padding:20px
+  }
+
+.container {
+    border: 1px solid steelblue;
+    padding: 30px;
+    max-width: 500px;
+    margin: auto;
+}
+
+button {
+    background-color: steelblue;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
 </style>
---
+++ VueJS1/src/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <h1 :style="{ color: color }">{{ title }}</h1>
+    <button @click="handleClick()" :style="{ background: bgColor }">{{ action }}</button>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    title: String,
+    action: String,
+    color: String,
+    bgColor: String
+  },
+  methods: {
+    handleClick() {
+      console.log('Button clicked!');
+    }
+  },
+}
+</script>

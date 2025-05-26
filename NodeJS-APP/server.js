5e96836a16647d635c3289b24fbdc415ad5c6c32
sanjaydevang <sanjayvdevang067@gmail.com>
2025-05-26 15:57:25 -0400

    backend update

--- NodeJS-APP/server.js
+++ NodeJS-APP/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-
+app.use('/', express.static(__dirname + '/'))
 
 app.listen(3001, function () {
     console.log('app listining on port 3001')
